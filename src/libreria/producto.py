a023403dadb0f4afa21a35992da0dfd057fd9194
Luisa Londoño <mlondono408@cue.edu.co>
2026-05-22 18:59:24 -0500

    REFACTOR: implementación del  precio base y código limpio

--- src/libreria/producto.py
+++ src/libreria/producto.py
@@ -2,8 +2,13 @@ class Producto:
 
     def __init__(self, nombre, precio_base):
 
-        if precio_base <= 0:
-            raise ValueError("El precio debe ser mayor que cero")
+        self.validar_precio(precio_base)
 
         self.nombre = nombre
         self.precio_base = precio_base
+
+    @staticmethod
+    def validar_precio(precio):
+
+        if precio <= 0:
+            raise ValueError("El precio debe ser mayor que cero")
