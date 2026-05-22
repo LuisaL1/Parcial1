d97d1c762bff3f3454be4bdf0b5aa191ef4072b4
Luisa Londoño <mlondono408@cue.edu.co>
2026-05-22 18:47:46 -0500

    GREEN: implementación del  precio base

--- src/libreria/producto.py
+++ src/libreria/producto.py
@@ -1,2 +1,9 @@
 class Producto:
-    pass
+
+    def __init__(self, nombre, precio_base):
+
+        if precio_base <= 0:
+            raise ValueError("El precio debe ser mayor que cero")
+
+        self.nombre = nombre
+        self.precio_base = precio_base
