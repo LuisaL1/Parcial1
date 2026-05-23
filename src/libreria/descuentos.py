62999cc95f102a02e378eb1243776e6801d17872
Luisa Londoño <mlondono408@cue.edu.co>
2026-05-22 19:16:53 -0500

    REFACTOR: mejora de validacion de  descuentos

--- src/libreria/descuentos.py
+++ src/libreria/descuentos.py
@@ -1,6 +1,11 @@
-def aplicar_descuento(precio, descuento):
+def validar_descuento(descuento):
 
     if descuento < 0 or descuento > 40:
         raise ValueError("Descuento inválido")
+
+
+def aplicar_descuento(precio, descuento):
+
+    validar_descuento(descuento)
 
     return precio - (precio * descuento / 100)
