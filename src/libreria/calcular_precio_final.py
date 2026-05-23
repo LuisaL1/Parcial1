7ed872456d60de37428247000ec451257113ffba
Luisa Londoño <mlondono408@cue.edu.co>
2026-05-22 19:24:34 -0500

    GREEN: implementacion minima para el calculo final

--- src/libreria/calcular_precio_final.py
+++ src/libreria/calcular_precio_final.py
@@ -1,2 +1,10 @@
+from src.libreria.descuentos import aplicar_descuento
+
+
 def calcular_precio_final(precio, descuento):
-    pass
+
+    precio_con_descuento = aplicar_descuento(precio, descuento)
+
+    precio_final = precio_con_descuento * 1.19
+
+    return precio_final
